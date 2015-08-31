bb34911e95e4cc27ee5a60ea7c353f5a2cd4c415
stivensanchez <stivensanchez003@gmail.com>
2015-08-31 21:36:19 +0000

    diseño costos show 31/08/2015

--- app/models/costo_general.rb
+++ app/models/costo_general.rb
@@ -1,2 +1,20 @@
 class CostoGeneral < ActiveRecord::Base
+    before_validation :calcular
+    
+    def calcular 
+        #Sueldos
+            self.total_sueldo = salario + subsidio
+            self.total_porcentaje = salario_porcentaje + subsidio_procentaje
+        #Costos Prestaciones(FALTA)
+        #Costos por Mes
+            self.salud = salario * porcenjatesalud
+            self.pension = salario * porcentajepension
+            self.rprofesional = salario * porcentajerprofesional
+            self.sena = salario * porcentajesena
+            self.icbf = salario * porcentajeicbf
+            self.cajacompensaciontotal = salario * porcentajecajacompensacion
+            self.total_costom = salud + pension + rprofesional + sena + icbf + cajacompensaciontotal
+            self.porcentajetotal_costom = porcenjatesalud + porcentajepension + porcentajerprofesional + porcentajesena + porcentajeicbf + porcentajecajacompensacion
+            self.porcentajetotalneto = porcentajetotal_costom + .. + total_porcentaje
+    end 
 end
